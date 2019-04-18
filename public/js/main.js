c1d30e466816103a2c47ccc8aaf2da73b6474753
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-04-18 17:31:05 +0300

    control required

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+/** FOR MENU **/
 (function($) {
     "use strict"; // Start of use strict
   
@@ -39,4 +40,25 @@
     }
   
   })(jQuery); // End of use strict
+
+
+/** FOR INVALID **/
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
   
